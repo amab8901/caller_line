02d207564673e12a4b7cb918db71a9f267db40b7
Amir Abdin <amab8901@protonmail.com>
2024-07-03 13:13:21 +0200

    refac: split the function

--- src/caller_line.rs
+++ src/caller_line.rs
@@ -2,6 +2,15 @@ use backtrace::{Backtrace, BacktraceFrame, BacktraceSymbol};
 
 pub fn caller_line() -> String {
     let (trace, curr_file, curr_line) = (Backtrace::new(), file!(), line!());
+    let backtrace_symbol = backtrace_symbol(trace, curr_file, curr_line);
+    let filename = filename(&backtrace_symbol);
+    let lineno = line_number(&backtrace_symbol);
+    let previous_line = format!("{filename}:{lineno}");
+
+    previous_line
+}
+
+fn backtrace_symbol(trace: Backtrace, curr_file: &str, curr_line: u32) -> Option<BacktraceSymbol> {
     let frames = trace.frames();
     let backtrace_symbol = frames
         .iter()
@@ -14,20 +23,23 @@ pub fn caller_line() -> String {
         })
         .nth(2)
         .cloned();
+    backtrace_symbol
+}
 
+fn filename(backtrace_symbol: &Option<BacktraceSymbol>) -> &str {
     let filename = backtrace_symbol
         .as_ref()
         .and_then(BacktraceSymbol::filename)
         .unwrap()
         .to_str()
         .unwrap();
+    filename
+}
 
-    let lineno = backtrace_symbol
+fn line_number(backtrace_symbol: &Option<BacktraceSymbol>) -> u32 {
+    let line_number = backtrace_symbol
         .as_ref()
         .and_then(BacktraceSymbol::lineno)
         .unwrap();
-
-    let previous_line = format!("{filename}:{lineno}");
-
-    previous_line
+    line_number
 }
